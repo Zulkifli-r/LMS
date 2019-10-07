cb77a43eedf795e8dc5beafbfa0629d91df35fff
zulkifli_r <muh.zul.r@gmail.com>
2019-10-07 16:45:37 +0800

    create classrooms related migration

---
+++ database/migrations/2019_10_07_030151_create_classrooms_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassroomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classrooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('subject_id')->unsigned();
+            $table->bigIncrements('teaching_period_id')->unsigned();
+            $table->string('slug')->unique();
+            $table->string('code')->unique();
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('start_at')->nullable();
+            $table->dateTime('end_at')->nullable();
+            $table->bigInteger('created_by')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classrooms');
+    }
+}
