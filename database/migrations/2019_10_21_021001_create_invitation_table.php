5f9964f97f2418663fb50a62fcd29dab31831e5d
zulkifli_r <muh.zul.r@gmail.com>
2019-10-22 13:42:32 +0800

    invitation's table migration

---
+++ database/migrations/2019_10_21_021001_create_invitation_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvitationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('type', ['public', 'private'])->default('private');
+            $table->enum('invited_as', ['student', 'teacher'])->default('student');
+            $table->bigInteger('classroom_id')->unsigned();
+            $table->string('email')->nullable();
+            $table->string('token')->unique()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invitation');
+    }
+}
