8c5111dd04718116f43888ad129f85847dc3bb4b
zulkifli_r <muh.zul.r@gmail.com>
2019-11-18 16:05:43 +0800

    added middleware to identify classroom users

---
+++ app/Http/Middleware/ClassroomResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Exceptions\AuthorizationException;
+use Closure;
+
+class ClassroomResource
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $classroom = \App\Classroom::getBySlug($request->slug);
+
+        if ( !$request->user('api') && !in_array($request->user('api')->id, $classroom->users->pluck('id')->toArray())  ) {
+            throw new AuthorizationException();
+        }
+
+        return $next($request);
+    }
+}
